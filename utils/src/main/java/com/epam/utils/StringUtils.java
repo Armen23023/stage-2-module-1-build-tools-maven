226152f02d4b0aa87cfd11572b500b44b62a54a3
Armen23023 <mirzoyana805@gmail.com>
2023-02-27 20:37:19 +0400

    WIP

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,7 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        double v = Double.parseDouble(str);
+        return v>0;
     }
 }
